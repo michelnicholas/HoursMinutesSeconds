a82fe3461fc4ec8c54e56972a98736f19dd5c5fd
Nicholas <zmichen@amazon.com>
2022-01-12 14:20:53 -0500

    added final constant value for invalid value

--- src/main/java/SecondsAndMinutes.java
+++ src/main/java/SecondsAndMinutes.java
@@ -1,5 +1,6 @@
 public class SecondsAndMinutes {
 
+    private static final String INVALID_VALUE_MESSAGE = "INVALID VALUE";
     public static void main(String[] args) {
         System.out.println(getDurationString(69,45));
         System.out.println(getDurationString(3945L));
@@ -12,18 +13,33 @@ public class SecondsAndMinutes {
 
 
         if ((minutes < 0) || (seconds < 0 ) || (seconds > 59) ){
-         return "Invalid Value";
+         return INVALID_VALUE_MESSAGE;
     }
         long hour = minutes / 60;
         long remainingMinutes = minutes % 60;
 
 
-        return  hour +  "h " + remainingMinutes + "m " + seconds + "s";
+        String hourString = hour + "h";
+        if (hour < 10){
+            hourString = "0" + hourString;
+        }
+
+        String minutesString = remainingMinutes + "m";
+        if (remainingMinutes < 10){
+            minutesString = "0" + minutesString;
+        }
+
+        String secondString = seconds + "s";
+        if (seconds < 10){
+            secondString = "0" + secondString;
+        }
+
+        return  hourString + " " + minutesString + " " + secondString + " ";
     }
 
     public static String getDurationString(long seconds){
         if (seconds < 0){
-            return "Invalid Value";
+            return INVALID_VALUE_MESSAGE;
         }
 
         long addedMinutes = seconds / 60;
